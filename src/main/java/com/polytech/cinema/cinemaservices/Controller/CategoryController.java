116ffa6e37d7e67825146308d31c4634279b3b49
Joymangul Jensen Selwyn <jensen.joymangul@etu.univ-lyon1.fr>
2017-10-13 13:16:29 +0200

    Complete CRUD for the category entity

--- src/main/java/com/polytech/cinema/cinemaservices/Controller/CategoryController.java
+++ src/main/java/com/polytech/cinema/cinemaservices/Controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.polytech.cinema.cinemaservices.Controller;
 
-import com.polytech.cinema.cinemaservices.model.Actor;
+
 import com.polytech.cinema.cinemaservices.model.Category;
-import com.polytech.cinema.cinemaservices.repo.ActeurRepository;
 import com.polytech.cinema.cinemaservices.repo.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,5 +20,47 @@ public class CategoryController {
     @GetMapping("")
     public List<Category> getAllCategory() {
         return categoryRepository.findAll();
+    }
+
+    // Get a category
+    @GetMapping("/{code}")
+    public ResponseEntity<Category> getCategoryByCode(@PathVariable(value = "code") String code) {
+        Category category = categoryRepository.findOne(code);
+        if(category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(category);
+    }
+
+    // Create a new Category
+    @PostMapping("")
+    public Category createActor(@Valid Category category) {
+        return categoryRepository.save(category);
+    }
+
+    // Update a category
+    @PutMapping("/{code}")
+    public ResponseEntity<Object> updateNote(@PathVariable(value = "code") String code,
+                                             @Valid  Category categoryDetails) {
+        Category category = categoryRepository.findOne(code);
+        if(category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        category.setName(categoryDetails.getName());
+
+        Category updatedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+
+    // Delete a category
+    @DeleteMapping("{code}")
+    public ResponseEntity<Object> deleteNote(@PathVariable(value = "code") String code) {
+        Category category = categoryRepository.findOne(code);
+        if(category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        categoryRepository.delete(category);
+        return ResponseEntity.ok().build();
     }
 }
